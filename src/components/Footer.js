5f036c7a7a3f7e2f418d9729d3de390373568657
Ocean Fuaga <oceansfuaga@gmail.com>
2020-09-26 15:09:02 -0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,27 @@ function Footer({ darkMode }) {
 	const useStyles = makeStyles((theme) => ({
 		root: {
 			flexGrow: 1,
-			height: '10vh',
+			height: '20vh',
 			backgroundColor: darkMode ? '#424242' : '#f4f4f4',
+			textAlign: 'center',
+		},
+		copy: {
+			position: 'relative',
+			top: '80%',
 		},
 	}));
 
 	const classes = useStyles();
 
+	const date = new Date();
+
 	return (
 		<Box>
-			<Paper className={classes.root} elevation={0} square />
+			<Paper className={classes.root} elevation={0} square>
+				<footer className={classes.copy}>
+					<small>&copy; {date.getUTCFullYear()} Ocean Fuaga</small>{' '}
+				</footer>
+			</Paper>
 		</Box>
 	);
 }
