1e5721e6a25b6a3235d9d58cd0ae2c441fcd37fb
Ocean Fuaga <oceansfuaga@gmail.com>
2020-10-30 18:17:28 -0700

    Fix warnings

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,7 +16,7 @@ const app = firebase.initializeApp({
 export const provider = new firebase.auth.GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithRedirect(provider).then(function(result) {
   // // This gives you a Google Access Token. You can use it to access the Google API.
   // const token = result.credential.accessToken;
   // // The signed-in user info.
